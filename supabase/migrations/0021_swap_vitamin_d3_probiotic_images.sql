a99c8101e73ff3bb3ba560fca22ff66caf9966c6
jila <jillaka37@gmail.com>
2026-07-18 02:02:29 -0400

    Swap Vitamin D3 and Probiotic Capsules shop images per request

---
+++ supabase/migrations/0021_swap_vitamin_d3_probiotic_images.sql
@@ -0,0 +1,2 @@
+update public.products set image_url = 'https://kxdhmzzswssqxfexfpot.supabase.co/storage/v1/object/public/product-images/probiotic-capsules.jpg' where slug = 'vitamin-d3';
+update public.products set image_url = 'https://kxdhmzzswssqxfexfpot.supabase.co/storage/v1/object/public/product-images/vitamin-d3.jpg' where slug = 'probiotic-capsules';
